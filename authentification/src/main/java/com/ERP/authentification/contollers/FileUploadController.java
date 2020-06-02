ca7348508d307331e07375bd4b4ab53e6a9af7cb
Amed FATNASSi (ENISo) <ahmed.fatnassi@eniso.u-sousse.tn>
2020-06-02 09:59:46 +0100

    fix post file

--- authentification/src/main/java/com/ERP/authentification/contollers/FileUploadController.java
+++ authentification/src/main/java/com/ERP/authentification/contollers/FileUploadController.java
@@ -1,28 +1,21 @@
 package com.ERP.authentification.contollers;
 
 import com.ERP.authentification.Models.DBFile;
-import com.ERP.authentification.Models.UploadFileResponse;
 import com.ERP.authentification.services.DBFileStorageService;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationArguments;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
-import java.util.logging.Logger;
 import java.util.stream.Collectors;
  // https://www.callicoder.com/spring-boot-file-upload-download-jpa-hibernate-mysql-database-example/
 @Controller
@@ -32,7 +25,7 @@ public class FileUploadController {
     private DBFileStorageService dbFileStorageService;
 
     @PostMapping(value = "/uploadFile" , produces = MediaType.APPLICATION_PDF_VALUE)
-    public UploadFileResponse uploadFile(@RequestParam("file") MultipartFile file) throws IOException {
+    public ResponseEntity<Resource> uploadFile(@RequestParam("file") MultipartFile file) throws IOException {
         DBFile dbFile = dbFileStorageService.storeFile(file);
         System.out.println("file 1");
         String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
@@ -42,12 +35,16 @@ public class FileUploadController {
         System.out.println("file 2");
         System.out.println(fileDownloadUri);
 
-        return new UploadFileResponse(dbFile.getFileName(), fileDownloadUri,
-                file.getContentType(), file.getSize());
+        /*return new UploadFileResponse(dbFile.getFileName(), fileDownloadUri,
+                file.getContentType(), file.getSize());*/
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(dbFile.getFileType()))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + dbFile.getFileName() + "\"")
+                .body(new ByteArrayResource(dbFile.getData()));
     }
 
     @PostMapping("/uploadMultipleFiles")
-    public List<UploadFileResponse> uploadMultipleFiles(@RequestParam("files") MultipartFile[] files) {
+    public List<ResponseEntity<Resource>> uploadMultipleFiles(@RequestParam("files") MultipartFile[] files) {
         return Arrays.asList(files)
                 .stream()
                 .map(file -> {
